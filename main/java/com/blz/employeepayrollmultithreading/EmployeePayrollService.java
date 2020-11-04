311df7de29b02af1a2d382b5aa675db78b06b1a4
sharale34 <sourabhharale8000@gmail.com>
2020-11-04 22:52:41 +0530

    Added: Welcome message is added

---
+++ main/java/com/blz/employeepayrollmultithreading/EmployeePayrollService.java
@@ -0,0 +1,10 @@
+package com.blz.employeepayrollmultithreading;
+
+import java.util.logging.Logger;
+
+public class EmployeePayrollService {
+	public static void main(String[] args) {
+		Logger log = Logger.getLogger(EmployeePayrollService.class.getName());
+		log.info("Welcome to the Cab Invoice Generator System.");
+	}
+}
